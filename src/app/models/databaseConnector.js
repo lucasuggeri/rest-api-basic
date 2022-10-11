15775f23b807056544200c760c9c5666f141b2e0
lucasuggeri <lucasuggeri@outlook.com>
2022-10-10 23:10:49 -0300

    databaseConnector comentado

--- src/app/models/databaseConnector.js
+++ src/app/models/databaseConnector.js
@@ -1,38 +1,49 @@
-import {MongoClient} from 'mongodb'
-import { UUID } from 'bson';
+import { MongoClient } from "mongodb";
+import { UUID } from "bson";
 
-class MongoConnector{
-    constructor(config){
-        this.database = config.db_database;
-        this.uri = `mongodb://${config.db_username}:${config.db_password}@${config.db_hostname}:${config.db_port}`;
-    }
-    async run(collection){
-        this.client = new MongoClient(this.uri, {
-            useUnifiedTopology: true,
-            pkFactory: { createPk: () =>  new UUID().toString() }
-          });
-        this.client.connect();
-        this.collection = this.client.db(this.database).collection(collection);
-    }
-    async find(data){
-        const [result] = await this.collection.find(data).toArray();
-        return result;
-
-    }
-    async findAll(data){
-        const result = await this.collection.find(data).toArray();
-        return result;
-    }
-    async insert(data){
-
-        return await this.collection.insertOne(data);
-    }
-    async remove(data){
-        return await this.collection.deleteOne(data);
-    }
-    async update(target,data){
-        return this.collection.replaceOne(target,data);
-    }
+class MongoConnector {
+  //cria o contrutor
+  constructor(config) {
+    //faz a propriedade database do mongodb receber o db_database de environment
+    this.database = config.db_database;
+    //faz a variável uri receber os dados de conexão do db
+    this.uri = `mongodb://${config.db_username}:${config.db_password}@${config.db_hostname}:${config.db_port}`;
+  }
+  //???
+  async run(collection) {
+    this.client = new MongoClient(this.uri, {
+      //???
+      useUnifiedTopology: true,
+      //cria uma Primary key a partir de um id único universal (?)
+      pkFactory: { createPk: () => new UUID().toString() },
+    });
+    //da o comando de conectar
+    this.client.connect();
+    //faz a variável collection receber a tabela collection do banco de dados database
+    this.collection = this.client.db(this.database).collection(collection);
+  }
+  //retorna uma informação da collection numa array
+  async find(data) {
+    const [result] = await this.collection.find(data).toArray();
+    return result;
+  }
+  //retorna a collection inteira numa array
+  async findAll(data) {
+    const result = await this.collection.find(data).toArray();
+    return result;
+  }
+  //insere dados na collection
+  async insert(data) {
+    return await this.collection.insertOne(data);
+  }
+  //remove dados da collection
+  async remove(data) {
+    return await this.collection.deleteOne(data);
+  }
+  //atualiza dados da collection
+  async update(target, data) {
+    return this.collection.replaceOne(target, data);
+  }
 }
 
 export default MongoConnector;
--- src/modules/speakers/speakersController.js
+++ src/modules/speakers/speakersController.js
@@ -1,44 +1,43 @@
-import speakersModel from '../../app/models/speakersModel.js';
+import speakersModel from "../../app/models/speakersModel.js";
 export default {
-    insert: async (req, res) => {
-        const result = await speakersModel.insert(req.body);
-        if (result.insertedId) {
-            const [inserted] = await speakersModel.findAll({
-                "_id": result.insertedId
-            })
-            res.status(201).send(inserted)
-        };
-        return res.status(400).send();
-    },
-    list: async (req, res) => {
-        const {
-            query
-        } = req;
-        let search = {}
-        if (query) {
-            search = query;
-            if (query.speakerId) search._id = query.speakerId;
-            delete search.speakerId;
-        }
-        return res.json(await speakersModel.findAll(search));
-    },
-    remove: async (req, res) => {
-        const result = await speakersModel.remove({
-            "_id": req.params.id
-        })
-        if (result.deletedCount) return res.status(204).send();
-        return res.status(400).send();
-    },
-    update: async (req, res) => {
-        const target = {
-            "_id": req.params.id
-        }
-        const newData = req.body
-        const result = await speakersModel.update(target, newData)
-        if (result.modifiedCount) {
-            const [inserted] = await speakersModel.findAll(target)
-            return res.status(200).send(inserted)
-        };
-        return res.status(400).send();
+  //faz insert no
+  insert: async (req, res) => {
+    const result = await speakersModel.insert(req.body);
+    if (result.insertedId) {
+      const [inserted] = await speakersModel.findAll({
+        _id: result.insertedId,
+      });
+      res.status(201).send(inserted);
     }
-}
+    return res.status(400).send();
+  },
+  list: async (req, res) => {
+    const { query } = req;
+    let search = {};
+    if (query) {
+      search = query;
+      if (query.speakerId) search._id = query.speakerId;
+      delete search.speakerId;
+    }
+    return res.json(await speakersModel.findAll(search));
+  },
+  remove: async (req, res) => {
+    const result = await speakersModel.remove({
+      _id: req.params.id,
+    });
+    if (result.deletedCount) return res.status(204).send();
+    return res.status(400).send();
+  },
+  update: async (req, res) => {
+    const target = {
+      _id: req.params.id,
+    };
+    const newData = req.body;
+    const result = await speakersModel.update(target, newData);
+    if (result.modifiedCount) {
+      const [inserted] = await speakersModel.findAll(target);
+      return res.status(200).send(inserted);
+    }
+    return res.status(400).send();
+  },
+};
